3e856be1d85a42da0628b6f07f9968e2e25640c5
Gardiner Nardini <gardiner.nardini@gmail.com>
2018-06-04 05:03:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments){
   var arraY=[];
-  for(i=0;i<musicians.length;i++){
+  for(var i=0;i<musicians.length;i++){
     arraY[i] = `${musicians[i]} plays the ${instruments[i]}`;
   }
 }
